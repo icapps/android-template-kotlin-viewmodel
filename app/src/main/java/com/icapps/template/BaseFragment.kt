d2d65ffa6db01a97180619ec0ed04b30046b6145
Maarten Van Giel <maarten.vangiel@icapps.com>
2018-05-23 09:37:59 +0200

    Make sure onsaveInstanceState can be used more than once, add getOrCreateActivityViewModel

--- app/src/main/java/com/icapps/template/BaseFragment.kt
+++ app/src/main/java/com/icapps/template/BaseFragment.kt
@@ -16,7 +16,7 @@ abstract class BaseFragment : DaggerFragment() {
     @Inject
     protected lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    protected val viewModels = mutableListOf<BaseViewModel>()
+    protected val viewModels = mutableMapOf<KClass<*>, BaseViewModel>()
 
     protected inline fun <reified T : BaseViewModel> getOrCreateViewModel(savedInstanceState: Bundle? = null): T {
         val viewModel = ViewModelProviders.of(this, viewModelFactory)[T::class.java]
@@ -25,14 +25,30 @@ abstract class BaseFragment : DaggerFragment() {
             viewModel.restoreInstanceState(it)
         }
 
-        viewModels.add(viewModel)
+        viewModels[T::class] = viewModel
+        return viewModel
+    }
+
+    protected inline fun <reified T : BaseViewModel> getOrCreateActivityViewModel(savedInstanceState: Bundle? = null): T {
+        val activity = activity ?: throw IllegalStateException("Fragment not attached to activity")
+        val viewModel = ViewModelProviders.of(activity, viewModelFactory)[T::class.java]
+
+        savedInstanceState?.let {
+            viewModel.restoreInstanceState(it)
+        }
+
+        viewModels[T::class] = viewModel
         return viewModel
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        viewModels.forEach { it.saveInstanceState(outState) }
-        viewModels.clear()
+        viewModels.forEach { it.value.saveInstanceState(outState) }
         super.onSaveInstanceState(outState)
+    }
+
+    override fun onDestroy() {
+        viewModels.clear()
+        super.onDestroy()
     }
 
 }
