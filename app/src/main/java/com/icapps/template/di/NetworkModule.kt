8b842aae132245376b3a76b9efac14f582613f7d
Nicola Verbeeck <nicola@chimerapps.com>
2020-12-02 12:23:12 +0100

    Remove bad .idea files Update dependencies and modernize gradle Update detekt config

--- app/src/main/java/com/icapps/template/di/NetworkModule.kt
+++ app/src/main/java/com/icapps/template/di/NetworkModule.kt
@@ -1,9 +1,12 @@
 package com.icapps.template.di
 
 import android.app.Application
+import com.icapps.architecture.utils.async.ScalingThreadPoolExecutor
 import com.icapps.niddler.core.AndroidNiddler
 import com.icapps.niddler.core.Niddler
 import com.icapps.niddler.interceptor.okhttp.NiddlerOkHttpInterceptor
+import com.icapps.niddler.retrofit.NiddlerRetrofitCallInjector
+import com.icapps.template.model.Constants
 import com.icapps.template.service.ExampleService
 import com.squareup.moshi.Moshi
 import dagger.Module
@@ -12,6 +15,8 @@ import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.Executor
+import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -22,12 +27,18 @@ import javax.inject.Singleton
 @Module
 class NetworkModule(private val baseUrl: String) {
 
+    private companion object {
+        private const val NIDDLER_TRACE_SKIP = 5
+        private const val NIDDLER_STACKTRACE_SIZE = 10
+    }
+
     @Provides
     @Singleton
     fun provideNiddler(application: Application): Niddler {
         val niddler = AndroidNiddler.Builder()
                 .setPort(0)
                 .setNiddlerInformation(AndroidNiddler.fromApplication(application))
+                .setMaxStackTraceSize(NIDDLER_STACKTRACE_SIZE)
                 .build()
         niddler.attachToApplication(application)
         return niddler
@@ -57,11 +68,18 @@ class NetworkModule(private val baseUrl: String) {
 
     @Provides
     @Singleton
-    fun provideRetrofit(moshi: Moshi, okHttpClient: OkHttpClient): Retrofit {
-        return Retrofit.Builder()
+    fun provideHttpExecutor(): Executor = ScalingThreadPoolExecutor(1, Constants.THREAD_POOL_MAX_THREAD_COUNT, Constants.THREAD_POOL_KEEP_ALIVE_SECONDS, TimeUnit.SECONDS)
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(moshi: Moshi, okHttpClient: OkHttpClient, niddler: Niddler, executor: Executor): Retrofit {
+        val builder = Retrofit.Builder()
                 .addConverterFactory(MoshiConverterFactory.create(moshi))
+                .callbackExecutor(executor)
                 .baseUrl(baseUrl)
                 .client(okHttpClient)
+
+        return NiddlerRetrofitCallInjector.inject(builder, niddler, okHttpClient, NIDDLER_TRACE_SKIP)
                 .build()
     }
 
--- app/src/main/java/com/icapps/template/model/Constants.kt
+++ app/src/main/java/com/icapps/template/model/Constants.kt
@@ -2,6 +2,10 @@ package com.icapps.template.model
 
 import java.util.concurrent.TimeUnit
 
+@Suppress("MagicNumber")
 object Constants {
     val DEFAULT_CACHE_VALIDITY = TimeUnit.MINUTES.toMillis(5)
+
+    const val THREAD_POOL_MAX_THREAD_COUNT = 10
+    const val THREAD_POOL_KEEP_ALIVE_SECONDS = 5L
 }
